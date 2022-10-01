ff5a64fafb6bdf3b95869ae9379f8d99e10f1df7
Kamil <kamil.mlynski1@gmail.com>
2022-10-01 19:37:35 +0200

    Log in test

---
+++ e2e/login-test.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  // Go to https://www.saucedemo.com/
+  await page.goto('https://www.saucedemo.com/');
+
+  // Click [data-test="username"]
+  await page.locator('[data-test="username"]').click();
+
+  // Fill [data-test="username"]
+  await page.locator('[data-test="username"]').fill('standard_user');
+
+  // Press Tab
+  await page.locator('[data-test="username"]').press('Tab');
+
+  // Click [data-test="password"]
+  await page.locator('[data-test="password"]').click();
+
+  // Fill [data-test="password"]
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+
+  // Click [data-test="login-button"]
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+  // Click .app_logo
+  await expect(page.locator('.app_logo')).toBeVisible();
+
+});
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 7,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -48,60 +48,61 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+        baseURL: 'https://www.saucedemo.com/'
       },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        ...devices['iPhone 12'],
+      },
+    },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
+    {
+      name: 'Microsoft Edge',
+      use: {
+        channel: 'msedge',
+      },
+    },
+    {
+      name: 'Google Chrome',
+      use: {
+        channel: 'chrome',
+      },
+    },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
    outputDir: 'test-results/',
 
-  /* Run your local dev server before starting the tests */
-    webServer: {
-    command: 'npm run start',
-    port: 3000,
-   },
+  // /* Run your local dev server before starting the tests */
+  //   webServer: {
+  //   command: 'npm run start',
+  //   port: 3000,
+  //  },
 };
 
 export default config;
