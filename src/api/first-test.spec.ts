4a060ff3a690d57f1d4fe5de0d58137f1472fca3
Kamil <kamil.mlynski1@gmail.com>
2022-10-19 20:50:15 +0200

    API test playwright

---
+++ src/api/first-test.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test'
+
+test('Get user details', async ({ request }) => {
+    const userDetails = await request.get(`https://reqres.in/api/users/2`)
+    expect(userDetails.ok()).toBeTruthy()
+    expect(userDetails.status()).toBe(200)
+    const result = await userDetails.json()
+    expect(result.data.email).toContain('janet')
+    expect(result.data.first_name).toBe('Janet')
+})
