97f75140b83a3a39349806877ed4bf4c1bb5c625
Kamil <kamil.mlynski1@gmail.com>
2023-01-27 14:31:26 +0100

    Test CI failed

--- src/api/first-test.spec.ts
+++ src/api/first-test.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('Get user details', async ({ request }) => {
     const userDetails = await request.get(`https://reqres.in/api/users/2`)
-    expect(userDetails.ok()).toBeTruthy()
+    expect(userDetails.ok()).not.toBeTruthy()
     expect(userDetails.status()).toBe(200)
     const result = await userDetails.json()
     expect(result.data.email).toContain('janet')
