1f7a8c5b4b8fb50da39288858028e662e11e0d26
Kamil <kamil.mlynski1@gmail.com>
2023-01-27 14:33:10 +0100

    config fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,10 @@ const config: PlaywrightTestConfig = {
         // baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry',
+        trace: 'on',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure'
+
         baseURL: 'https://www.saucedemo.com',
     },
 
