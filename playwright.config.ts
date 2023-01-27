f899b041f108153dbc55125fa2a8ad914cea3a76
Kamil <kamil.mlynski1@gmail.com>
2023-01-27 14:33:32 +0100

    config fix 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ const config: PlaywrightTestConfig = {
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on',
         screenshot: 'only-on-failure',
-        video: 'retain-on-failure'
+        video: 'retain-on-failure',
 
         baseURL: 'https://www.saucedemo.com',
     },
